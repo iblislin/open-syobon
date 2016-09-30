d2173008125b7f7b580da90d8dec7bf8a5a23767
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-09-30 20:20:43 +0800

    Fix indentation of `main` via vim

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,26 @@ int main(int argc, char *argv[])
 {
     parseArgs(argc, argv);
     if (DxLib_Init() == -1)
-	return 1;
+        return 1;
 
-//全ロード
+    //全ロード
     loadg();
 
-//フォント
+    //フォント
     SetFontSize(16);
-//SetFontThickness(4) ;
+    //SetFontThickness(4) ;
 
-//ループ
-//for (maint=0;maint<=2;maint++){
+    //ループ
+    //for (maint=0;maint<=2;maint++){
     while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
-	UpdateKeys();
-	maint = 0;
-	Mainprogram();
-	if (maint == 3)
-	    break;
+        UpdateKeys();
+        maint = 0;
+        Mainprogram();
+        if (maint == 3)
+            break;
     }
 
-//ＤＸライブラリ使用の終了処理
+    //ＤＸライブラリ使用の終了処理
     end();
 }
 
