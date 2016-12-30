311765cba0990899feacdaf76e726214a3512925
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-12-30 22:04:29 +0800

    [DxLib] refine DerivationGraph

--- src/DxLib.cpp
+++ src/DxLib.cpp
@@ -228,14 +228,14 @@ void DrawVertTurnGraph(int a, int b, SDL_Surface * mx)
     }
 }
 
-SDL_Surface *DerivationGraph(int srcx, int srcy, int width, int height,
-			     SDL_Surface * src)
+SDL_Surface* DerivationGraph(int srcx, int srcy, int width, int height,
+                             SDL_Surface * src)
 {
-    SDL_Surface *img =
-	SDL_CreateRGBSurface(SDL_SWSURFACE, width, height,
-			     screen->format->BitsPerPixel,
-			     src->format->Rmask, src->format->Bmask,
-			     src->format->Gmask, src->format->Amask);
+    SDL_Surface *img = SDL_CreateRGBSurface(
+        SDL_SWSURFACE, width, height,
+        screen->format->BitsPerPixel,
+        src->format->Rmask, src->format->Bmask,
+        src->format->Gmask, src->format->Amask);
 
     SDL_Rect offset;
     offset.x = srcx;
@@ -245,7 +245,7 @@ SDL_Surface *DerivationGraph(int srcx, int srcy, int width, int height,
 
     SDL_BlitSurface(src, &offset, img, NULL);
     SDL_SetColorKey(img, SDL_SRCCOLORKEY,
-		    SDL_MapRGB(img->format, 9 * 16 + 9, 255, 255));
+                    SDL_MapRGB(img->format, 9 * 16 + 9, 255, 255));
     return img;
 }
 
