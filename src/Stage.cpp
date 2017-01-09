41b4e5f2267e9cab7392c8c0544b140959bb40f5
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-01-09 13:30:34 +0800

    [stage] extract 1-2-2

--- src/Stage.cpp
+++ src/Stage.cpp
@@ -75,6 +75,7 @@ void StageInfo::init_map()
 	this->maps[std::make_tuple(1, 1, 0)] = new StageMap("stage/1-1-0.json");
 	this->maps[std::make_tuple(1, 2, 0)] = new StageMap("stage/1-2-0.json");
 	this->maps[std::make_tuple(1, 2, 1)] = new StageMap("stage/1-2-1.json");
+	this->maps[std::make_tuple(1, 2, 2)] = new StageMap("stage/1-2-2.json");
 }
 
 
